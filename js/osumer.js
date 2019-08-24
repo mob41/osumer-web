e0ecc2887a85c7eec7011c282646c8d53c4f564d
mob41 <puihuenlaw@gmail.com>
2019-08-24 18:21:13 +0800

    Add GA

--- js/osumer.js
+++ js/osumer.js
@@ -81,6 +81,13 @@ $(".view-song-footer .col-sm-3").on("click", function () {
     }
 });
 
+function about() {
+    $(".modal-header").html("<h5 class=\"modal-title\" id=\"modalLabel\">About</h5><button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>");
+    $(".modal-body").html("<p>osumer2 is an application that allows osu! players download and search beatmaps conveniently. This website allows users to search for any beatmaps that is cached by all osumer clients previously.</p><p>You might not be able to find some beatmaps in this website because some of them might not be already cached and uploaded by our clients.</p>");
+    $(".modal-footer").html("<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>");
+    $(".modal").modal();
+}
+
 function adjustMapListMargin() {
     var hh = $(".header").height();
     var fh = $(".footer").height();
@@ -213,6 +220,9 @@ function processUrl() {
     var keywords = paras["q"];
     var page = paras["p"];
     var id = paras["id"];
+    if (keywords === "") {
+        hideInfo();
+    }
     if (keywords) {
         hideInfo();
 
